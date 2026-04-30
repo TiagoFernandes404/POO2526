ea272f5b12b822fbe9d97748af9b63b183034222
TiagoFernandes404 <162730263+TiagoFernandes404@users.noreply.github.com>
2026-04-30 19:29:57 +0100

    Add files via upload
    Corrigi alguns erros de encampulamento e comentei algum codigo so falta o controller e as views

---
+++ Main.java
@@ -0,0 +1,11 @@
+package domuscontrol;
+
+import domuscontrol.controller.ControllerTotal;
+
+// MAin serve para arrancarmos o progrmaa
+public class Main {
+
+    public static void main(String[] args) {
+        new ControllerTotal().run();
+    }
+}
